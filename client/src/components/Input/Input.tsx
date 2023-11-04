1221d735a6c24e9e671a1c7ee419965c6f07bc21
Harvey Yerik <yerik05vh@gmail.com>
2023-11-03 19:12:54 -0500

    :poop: Custom hook need improve.

--- client/src/components/Input/Input.tsx
+++ client/src/components/Input/Input.tsx
@@ -2,16 +2,26 @@ import st from "./Input.module.css";
 import { InputProps } from "../../types/props";
 import { useState } from "react";
 import { useAppSelector } from "../../redux/hook";
+import useValidatinoInput from "../../hooks/useValidationInput";
 
 function Input({ label, name, type, placeholder, onChange }: InputProps) {
   const [typeInput, setTypeInput] = useState(type);
+  const [input, setInput] = useState("");
+  const { error, message, validateGeneral } = useValidatinoInput({
+    type: type,
+  });
   const mode = useAppSelector((state) => state.modeReducer.mode);
   const switchType = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
     if (typeInput === "password") setTypeInput("text");
     else setTypeInput("password");
   };
-
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const text = e.target.value;
+    setInput(text);
+    onChange(e);
+    validateGeneral(text);
+  };
   return (
     <div className={`${st[mode]} ${st.input}`}>
       <label>{label}</label>
@@ -19,7 +29,7 @@ function Input({ label, name, type, placeholder, onChange }: InputProps) {
         type={typeInput}
         name={name}
         placeholder={placeholder}
-        onChange={(e) => onChange(e)}
+        onChange={(e) => handleChange(e)}
         onFocus={(e) => onChange(e)}
         autoComplete="false"
         autoFocus
@@ -33,6 +43,7 @@ function Input({ label, name, type, placeholder, onChange }: InputProps) {
           <a>Forgot your password?</a>
         </>
       )}
+      {error && <p>{message}</p>}
     </div>
   );
 }
--- client/src/hooks/useValidationInput.ts
+++ client/src/hooks/useValidationInput.ts
@@ -1,37 +1,95 @@
 import { useState } from "react";
 
 const MESSAGES = {
-  lengthMin: "",
-  lengthMax: "",
+  lengthMin: "El minimo de caracteres es de 8.",
+  lengthMax: "El maximo de caracteres es de 30.",
+};
+const TYPES = {
+  password: "password",
+  email: "email",
+  text: "text",
 };
 interface useValidatinoInputInterface {
   type: string;
-  regex?: RegExp;
+  regex?: { regex: RegExp; message: string }[];
   max?: number;
   min?: number;
 }
+const REGEX_DEFAULT = {
+  email: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/],
+  password: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/],
+  text: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/],
+};
 function useValidatinoInput({
   type,
   regex,
   max = 30,
-  min = 6,
+  min = 8,
 }: useValidatinoInputInterface) {
   const [error, setError] = useState(false);
   const [message, setMessage] = useState("");
 
   const validateGeneral = (value: string) => {
     setError(true);
-    validateLength(value);
+    // validateLength(value);
+    validateRegexByType(value, type);
+    // validateCustomRegex(value, regex);
   };
+
   const validateLength = (value: string) => {
-    if (value.length < min) {
+    const lengthValue = value.length + 1;
+    if (lengthValue <= min) {
       setMessage(MESSAGES.lengthMin);
-    } else if (value.length > max) {
+    } else if (lengthValue >= max) {
       setMessage(MESSAGES.lengthMax);
     } else {
+      console.log("SE falsifica");
       setError(false);
     }
   };
+
+  const validateRegexByType = (value: string, type: string) => {
+    setError(true);
+    if (type === TYPES.email) {
+      validateListRegex(value, REGEX_DEFAULT.email, "Email incorrecto");
+    } else if (type === TYPES.password) {
+      validateListRegex(value, REGEX_DEFAULT.password, "Contraseña incorrecto");
+    } else if (type === TYPES.text) {
+      validateListRegex(value, REGEX_DEFAULT.text, "Texto incorrecto");
+    }
+  };
+
+  const validateListRegex = (
+    value: string,
+    regexList: RegExp[],
+    message: string
+  ) => {
+    const regexValidate = regexList.some((regex) => {
+      if (!regex.test(value)) {
+        setMessage(message);
+        return true;
+      } else {
+        return false;
+      }
+    });
+    if (!regexValidate) setError(false);
+  };
+
+  const validateCustomRegex = (
+    value: string,
+    regexs?: { regex: RegExp; message: string }[]
+  ) => {
+    setError(true);
+    if (regexs) {
+      regexs.forEach((item) => {
+        if (!item.regex.test(value)) {
+          console.log("Error de custom regex");
+        }
+      });
+    }
+    setError(false);
+  };
+
   return { error, message, validateGeneral };
 }
 export default useValidatinoInput;
